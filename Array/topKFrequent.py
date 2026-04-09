310a892a9feb83ae41c9188fa7ea909f53c05892
Aadesh Parija <60508886+aadesh1214@users.noreply.github.com>
2026-04-09 07:13:40 +0530

    Add topKFrequent function to find frequent elements
    Implement a solution to find the top K frequent elements in a list.

---
+++ Array/topKFrequent.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count={}
+        
+        for i in nums:
+            count[i]=count.get(i,0)+1
+
+        freq = [[] for i in range(len(nums)+1)]
+
+        for i , c in count.items():
+            freq[c].append(i)
+
+        res = []
+        for i in range(len(freq)-1,-1,-1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+            
+
+            
+        
